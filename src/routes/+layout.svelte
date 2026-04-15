38c096a28920d236163486538074e2c255e89e66
Ashwini Gurbaxani <98401812+gurbaxani@users.noreply.github.com>
2026-04-15 12:36:45 +0530

    current route indicator on dock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Pathname } from '$app/types';
 	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
+	import { page } from '$app/state'; // In Svelte 5, this is a reactive object
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
@@ -9,12 +9,15 @@
 	import { goto } from '$app/navigation';
 
 	let { children } = $props();
+
+	const isActive = (path: string) => page.url.pathname === path;
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<title>Jai Shree Ram</title>
 </svelte:head>
+
 {@render children()}
 
 <div style="display:none">
@@ -24,17 +27,17 @@
 </div>
 
 <div class="dock dock-xl">
-	<button onclick={() => goto('/read')}>
+	<button onclick={() => goto('/read')} class={isActive('/read') ? 'dock-active' : ''}>
 		<i class="ph-duotone ph-book-open-text text-xl"></i>
 		<span class="dock-label">Read</span>
 	</button>
 
-	<button class="dock-active" onclick={() => goto('/learn')}>
+	<button onclick={() => goto('/learn')} class={isActive('/learn') ? 'dock-active' : ''}>
 		<i class="ph-duotone ph-hands-praying text-xl"></i>
 		<span class="dock-label">Learn</span>
 	</button>
 
-	<button onclick={() => goto('/settings')}>
+	<button onclick={() => goto('/settings')} class={isActive('/settings') ? 'dock-active' : ''}>
 		<i class="ph-duotone ph-gear-six text-xl"></i>
 		<span class="dock-label">Settings</span>
 	</button>
