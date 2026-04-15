ec061f276b3a729075d7eb609f34be4c1006e499
Ashwini Gurbaxani <98401812+gurbaxani@users.noreply.github.com>
2026-04-15 11:31:00 +0530

    Jai Shree Ram

---
+++ src/routes/demo/paraglide/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { setLocale } from '$lib/paraglide/runtime';
+	import { m } from '$lib/paraglide/messages.js';
+</script>
+
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+
+<div>
+	<button onclick={() => setLocale('en')}>en</button>
+	<button onclick={() => setLocale('hi')}>hi</button>
+	<button onclick={() => setLocale('mr')}>mr</button>
+	<button onclick={() => setLocale('gu')}>gu</button>
+	<button onclick={() => setLocale('ta')}>ta</button>
+	<button onclick={() => setLocale('te')}>te</button>
+	<button onclick={() => setLocale('kn')}>kn</button>
+	<button onclick={() => setLocale('bn')}>bn</button>
+	<button onclick={() => setLocale('bho')}>bho</button>
+</div>
+
+<p>
+	If you use VSCode, install the
+
+	<a
+		href="https://marketplace.visualstudio.com/items?itemName=inlang.vs-code-extension"
+		target="_blank">Sherlock i18n extension</a
+	>
+
+	for a better i18n experience.
+</p>
